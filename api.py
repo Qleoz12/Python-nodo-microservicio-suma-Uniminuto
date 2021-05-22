b6b395a94ecc95fa6334ad6e1ee5888d2e8c9822
qleoz12 <kintaroloop@gmail.com>
2021-05-22 15:35:54 -0500

    ajuste de api explicacion

--- api.py
+++ api.py
@@ -1,12 +1,18 @@
 from flask import Flask, app
 import flask
+from flask import request
 
 app= Flask(__name__)
 i = 0
 listanumeros = []
 
-@app.route('/validacion/<tamaño:int>')
-tamaño = int
+#request param
+@app.route('/numero/agregar')
+def agregar():
+    numero = request.args.get('numero')
+    print(numero)
+
+'''tamaño = int
 tamaño = int(input("por favor digite el numero de digitos a sumar : "))   
 class Guardar_numero ():
   for i in range(tamaño):
@@ -22,6 +28,11 @@ def sumalista(listanumeros):
         return sumaTotal    
 
         print(sumalista(listanumeros))
+'''
+
+
+
+## inicio explico
 
 if __name__ == "__main__":
     app.run('127.0.0.2', '5005', debug=True)
