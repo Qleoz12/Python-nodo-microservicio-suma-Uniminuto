e7a83b81ec6dceda7bd5a80307e81a352a7d0819
Eduard98-u <84585186+Eduard98-u@users.noreply.github.com>
2021-05-24 20:17:36 -0500

    Update api.py

--- Nodo1/api.py
+++ Nodo1/api.py
@@ -1,15 +1,16 @@
-from flask import Flask, app
+from flask import Flask, jsonify, json, app
 from flask import request
 
 app= Flask(__name__)
 listanumeros = []
 
 #Funcion suma 
-@app.route('/numero/agregar')
-def agregar():
-    numero = request.args.get('numero')
-    print(numero)
-
+@app.route('/agregar',methods=['GET'])
+def Agregar():
+    suma = 0 
+    for n in listanumeros:
+       suma = suma + n
+    return jsonify('Suma del servidor: ' +suma)
 
 #Funcion para insertar 
 
@@ -19,5 +20,8 @@ def agregar():
     print(numero)
 
 
+#funcion para traer la suma de las listas 
+
 if __name__ == "__main__":
-    app.run('127.0.0.2', '5005', debug=True)
+
+    app.run(host='0.0.0.0', port='2000', debug=True)
