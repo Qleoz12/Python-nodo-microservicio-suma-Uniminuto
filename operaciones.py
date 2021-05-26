3f57b7dd238bb876a224a3c64054c172f215bea5
unknown <elozadaalba@uniminuto.edu.co>
2021-05-26 00:35:29 -0500

    Hice la funcion para recibir las sumas de los otros nodos meterlos en un arreglo y la funcion para sumarlas

--- operaciones.py
+++ operaciones.py
@@ -3,19 +3,33 @@ class Operaciones:
 
     def __init__(self):
         self.i = 0
-        #arreglo donde se guarda los numeros por nodo
+        #Lista donde se guarda los numeros por nodo
         self.listanumeros = []
+        self.listasumas = []
         
-
+    #Funcion para enviar los numeros a la lista obtenidos por parametro
     def saveNum(self,numero):
         self.listanumeros.append(numero)
         print(self.listanumeros)
         
-
+    #Funcion para realizar la suma de los numeros de la lista
     def sumNums (self):
         self.suma = 0
         for i in self.listanumeros:
             self.suma += i
-            
+    #Se imprime la suma guardada en la variable suma      
         print(self.suma)
         return self.suma
+    
+    #Recibo por parametros las sumas de los demas nodos y armo un arreglo
+    def saveNumNet(self,suma):
+        self.listasumas.append(suma)
+        print(self.listasumas)
+    #Sumamos los numeros recibidos de las sumas de los demas nodos    
+    def sumNumsNet (self):
+        self.sumaNet = 0
+        for i in self.listasumas:
+            self.sumaNet += i
+        print(self.sumaNet)
+        return self.sumaNet
+    
--- pruebas.py
+++ pruebas.py
@@ -16,4 +16,10 @@ obj.saveNum(3)
 obj.saveNum(3)
 obj.sumNums()
 
+obj.saveNumNet(18)
+obj.saveNumNet(21)
+obj.saveNumNet(27)
+obj.sumNumsNet()
+
+
     
