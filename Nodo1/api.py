608cb059cd4d52b9361d93aa2f395b588030f04c
Eduard98-u <84585186+Eduard98-u@users.noreply.github.com>
2021-05-26 21:10:15 -0500

    Update api.py

--- Nodo1/api.py
+++ Nodo1/api.py
@@ -1,35 +1,35 @@
 from flask import Flask, jsonify, json, app
 from flask import request
+from operaciones import Operaciones
 
 app= Flask(__name__)
 listanumeros = []
+_operaciones=Operaciones()
 
 #Funcion suma 
-@app.route('/Numero', methods=['GET'])
-def Lista ():
-    suma = 0 
-    for z in listanumeros:
-       suma = suma + z
-    return int(jsonify('Suma del servidor: ' +suma))
+@app.route('/Numero/<datoNumero>', methods=['GET'])
+def Lista (datoNumero):
+    #capturar el numero por la api
+    _operaciones.saveNum(datoNumero )
+    return jsonify('se ha guardado el numero '+ datoNumero),200
 
 #Funcion para insertar 
-@app.route('/Agregar', methods=['POST'])
-def Agregar():
-    listanumeros.append(request.json['numeros'])
-    return jsonify({'Numero almacenado '})
+@app.route('/sumar', methods=['GET'])
+def sumar():
+    sumaNumeros = _operaciones.sumNums()
+    return jsonify('la suma de numero'+str(sumaNumeros)),200
 
-#funcion para traer la suma de las listas 
-@app.route('/Traer', methods=['GET'])
-def Traer():
-    informaciones = request.get("https://localhost:3000/Numero")
-    suma = 0
-    for i in listanumeros:
-        suma = suma + i
-    sumaNodo = []
-    sumaNodo.append({'suma de los numeros: ': suma})  
-    sumaNodo.append(json.loads(informaciones.content))
-    return jsonify('valor total: '+ sumaNodo) 
-    
+#Recibo por parametros las sumas de los demas nodos y armo un arreglo
+@app.route('/sumaTotal', methods=['GET'])    
+def sumatotal():
+    sumatotal =_operaciones.sumNumNet()
+    return jsonify('la suma total de los nodos es: '+ str(sumatotal)),200
+
+#Sumamos los numeros recibidos de las sumas de los demas nodos   
+@app.route('/Total', methods=['GET'])
+def Total():
+    TotalSuma= _operaciones.sumNumsNet()
+    return jsonify('la operacion total de nodos son: '+ str(TotalSuma)),200
 
 if __name__ == "__main__":
 
